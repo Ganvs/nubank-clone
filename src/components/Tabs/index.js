ed2808068cc1a5f59282b1b4d89edab6d1cf9d14
Ganvs <gustavo.anverze@gmail.com>
2019-10-18 22:15:21 -0300

    Tabs components

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-import {Container} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Container, TabsContainer, TabItem, TabText} from './styles';
 
 export default function Tabs() {
-  return <Container />;
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#fff" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
 }
