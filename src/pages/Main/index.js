2394b7e8d170b474314b1d45f63fd6aa049dcf57
Ganvs <gustavo.anverze@gmail.com>
2019-10-21 19:51:35 -0300

    Card component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import {Container} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 1.975.046,79</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 2.950,00 recebida de Hotmart hoje às 12:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
